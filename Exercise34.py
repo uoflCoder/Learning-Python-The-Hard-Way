9f5d36a05aa73481cb3a96a20b634f8b1c916acf
Stephen Cornett <stephen.t.cornett@outlook.com>
2017-10-08 10:47:39 -0400

    Exercises 33 through 35 Completed

---
+++ Exercise34.py
@@ -0,0 +1,12 @@
+#Exercise 34
+
+animal = ['bear', 'python3.6','peacock', 'kangaroo', 'whale', 'platypus']
+
+print(animal[1])
+print(animal[2])
+print(animal[0])
+print(animal[3])
+print(animal[4])
+print(animal[2])
+print(animal[5])
+print(animal[4])
