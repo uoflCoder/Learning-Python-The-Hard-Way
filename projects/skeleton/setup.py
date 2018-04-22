c914803b8fce286442fa246cb66227c70ddc89ab
uoflCoder <tylorcornett@gmail.com>
2018-04-22 11:08:36 -0400

    Exercises 46 and 47 complete

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,21 @@
+try:
+	from setuptools import setup
+
+except ImportError:
+	from distutils.core import setup
+
+	config = {
+		'description': 'My Project',
+		'author': 'My Name',
+		'url': 'URL to get it at.',
+		'download_url': 'Where to download it',
+		'author_email': 'tylorcornett@gmail.com',
+		'version': '0.1',
+		'install_requires': ['nose'],
+		'packages': ['NAME'],
+		'scripts': [],
+		'name': 'Exercise 46'
+
+		}
+
+	setup(**config)
